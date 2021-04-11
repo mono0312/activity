e92f4bdf2b8faca5dd1ca0faf3842d83fe53b315
mono031200 <mono031200@gmail.com>
2021-04-10 20:05:02 -0500

    Initial commit

---
+++ database/migrations/2021_04_11_002453_create_stores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',45);
+            $table->string('address',45);
+            $table->string('phone',45);
+            $table->string('nit',45);
+            $table->string('owner_name',45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
---
+++ database/migrations/2021_04_11_002545_create_product_store_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_store', function (Blueprint $table) {
+            $table->id();
+            $table->increments('amount');
+            $table->string('product_storecol',45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_store');
+    }
+}
